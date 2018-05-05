8e40f115cf9b43948e88fccee958ccc0ef42971a
Mithilesh Sinha <mithilesh@mithileshs-macbook-air.local>
2018-05-06 01:18:12 +0530

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Mithilesh Sinha on 06/05/18.
+//  Copyright © 2018 mirab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
